361d87175d44b2553df6d87a85176cff5b28118c
SandroAnugrah <sandroanugrahtambunan6@gmail.com>
2025-01-06 22:39:58 +0700

    Meperbarui File Data Pekerjaan

--- src/components/pekerjaan/Data.jsx
+++ src/components/pekerjaan/Data.jsx
@@ -2,28 +2,29 @@ import Gambar1 from "../../assets/basarnas.png";
 import Gambar2 from "../../assets/fsi.png";
 import Gambar3 from "../../assets/ndaru.png";
 import Gambar4 from "../../assets/sarjana.png";
+import Gambar5 from "../../assets/ptsp.png";
 
 export const dataProyek = [
   {
     id: 1,
     gambar: Gambar1,
-    nama: "Website Portofolio",
+    nama: "Website Basarnas",
     kategori: "Website",
-    tautan: "https://github.com/fifovalle/WEBSITE-PORTOFOLIO-PRIBADI.git",
+    tautan: "https://github.com/sandroanugrah/BASARNAS",
   },
   {
     id: 2,
     gambar: Gambar2,
-    nama: "Website Portofolio",
+    nama: "Website FSI",
     kategori: "Website",
-    tautan: "https://github.com/fifovalle/WEBSITE-PORTOFOLIO-PRIBADI.git",
+    tautan: "https://github.com/sandroanugrah/FSI",
   },
   {
     id: 3,
     gambar: Gambar3,
-    nama: "Website Portofolio",
+    nama: "Website Ndaru Farm",
     kategori: "Website",
-    tautan: "https://github.com/fifovalle/WEBSITE-PORTOFOLIO-PRIBADI.git",
+    tautan: "https://github.com/sandroanugrah/NDARU-FARM",
   },
   {
     id: 4,
@@ -31,6 +32,13 @@ export const dataProyek = [
     nama: "Website Sarjana Geprek",
     kategori: "Website",
     tautan: "https://github.com/sandroanugrah/SARJANA-GEPREK",
+  },
+  {
+    id: 5,
+    gambar: Gambar5,
+    nama: "Website PTSP BMKG BENGKULU",
+    kategori: "Website",
+    tautan: "https://github.com/sandroanugrah/PTSP-BMKG-MAIN",
   },
 ];
 
